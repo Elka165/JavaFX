04033a929937f3c847954b952eb5257a0b83661a
Elka165 <ela_dolna99@o2.pl>
2020-05-04 20:55:17 +0200

    add args

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,6 +8,9 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import java.util.List;
+import java.util.Map;
+
 public class Main  extends Application{
     public static void main(String[] args) {
         Application.launch(args);
@@ -17,18 +20,30 @@ public class Main  extends Application{
     @Override
     public void start(Stage stage){
         try {
-            Label label=new Label("Witaj, świecie");
-            VBox root=new VBox();
-            Button stopButton=new Button("Żegnaj świecie");
-            stopButton.setOnAction(e->{
-                System.out.println("Kliknięto przycisk");
-                Platform.exit();
-            });
-//            StackPane root=new StackPane();
-            root.getChildren().addAll(label, stopButton);
-            Scene scene =new Scene(root, 300,200);
+//            1)
+//            Label label=new Label("Witaj, świecie");
+//            VBox root=new VBox();
+//            Button stopButton=new Button("Żegnaj świecie");
+//            stopButton.setOnAction(e->{
+//                System.out.println("Kliknięto przycisk");
+//                Platform.exit();
+//            });
+//            //dowolny rozkład przycisków
+//            //StackPane root=new StackPane();
+//            root.getChildren().addAll(label, stopButton);
+//            Scene scene =new Scene(root, 300,200);
+//            stage.setScene(scene);
+//            stage.show();
+
+//            2)
+
+            Parameters pp=this.getParameters();
+            List<String> unParams= pp.getUnnamed();
+            StackPane root = new StackPane();
+            Scene scene = new Scene(root, Double.parseDouble(unParams.get(0)), Double.parseDouble(unParams.get(1)));
             stage.setScene(scene);
             stage.show();
+
         }catch (Exception e){
             e.printStackTrace();
         }
